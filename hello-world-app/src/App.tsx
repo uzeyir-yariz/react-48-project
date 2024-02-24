8a548690198360fd8bb4b043a6efeabb0b135d2f
= <=>
2024-02-24 20:09:16 +0300

    first commit

---
+++ hello-world-app/src/App.tsx
@@ -0,0 +1,12 @@
+import HelloWorld from "./TS/helloWorld"
+
+const App = () => {
+  return (
+    <div>
+      <HelloWorld /> {/* HelloWorld bileşenini JSX içinde bu şekilde kullanabilirsiniz */}
+    </div>
+  );
+};
+
+
+export default App
---
+++ hello-world-app/src/TS/helloWorld.tsx
@@ -0,0 +1,9 @@
+const helloWorld = () => {
+  return (
+    <div className="alert alert-info">
+        <h1 className="text-center">HELLO WORLD</h1>      
+    </div>
+  )
+}
+
+export default helloWorld
